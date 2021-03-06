383823ec54de7bb48f5ffbe5702e49e98c41921b
test <test@online.com>
2021-03-06 03:11:50 +0000

    Publish to cloud

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -164,7 +164,7 @@ def show_exam_result(request, course_id, submission_id):
     context = {'course' : course,
                'submission' : submission,
                'choices' : choices,
-               'grade' : grade }
+               'grade' : int(grade) }
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
